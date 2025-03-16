2a11645e58a346bd3f626b0aa16865af7f5131b9
anranlyu <anranlyu@gmail.com>
2025-03-16 13:58:29 -0700

    Setting up the frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <p className="text-blue-600/100 dark:text-sky-400/100">
+        The quick brown fox...
+      </p>
+      <p className="text-blue-600/75 dark:text-sky-400/75">
+        The quick brown fox...
+      </p>
+      <p className="text-blue-600/50 dark:text-sky-400/50">
+        The quick brown fox...
+      </p>
+      <p className="text-blue-600/25 dark:text-sky-400/25">
+        The quick brown fox...
+      </p>
+    </>
+  );
+}
+
+export default App;
